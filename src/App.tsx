36303a2a571723f42dacc0f21ee0b74c85ea7461
LevelSchema <drain2291@gmail.com>
2024-08-15 15:58:00 +0900

    次月先月ボタンの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Report from './pages/Report';
-import NoMatch from './pages/NoMatch';
-import AppLayout from './components/layout/AppLayout';
-import { theme } from './theme/theme';
-import { ThemeProvider } from '@emotion/react';
-import { CssBaseline } from '@mui/material';
-import { Transaction } from './types/index';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Report from "./pages/Report";
+import NoMatch from "./pages/NoMatch";
+import AppLayout from "./components/layout/AppLayout";
+import { theme } from "./theme/theme";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import { Transaction } from "./types/index";
 import {
   addDoc,
   collection,
@@ -16,17 +16,17 @@ import {
   doc,
   deleteDoc,
   updateDoc,
-} from 'firebase/firestore';
-import { db } from './firebase';
-import { formatMonth } from './utils/formatting';
-import { Schema } from './validations/schema';
+} from "firebase/firestore";
+import { db } from "./firebase";
+import { formatMonth } from "./utils/formatting";
+import { Schema } from "./validations/schema";
 
 function App() {
   // firestoreエラーかどうかを判定する型ガード
   function isFireStoreError(
     err: unknown
   ): err is { code: string; message: string } {
-    return typeof err === 'object' && err !== null && 'code' in err;
+    return typeof err === "object" && err !== null && "code" in err;
   }
 
   // ※読み込むときはTransaction型、書き込むときはバリデーションつきのSchema型
@@ -36,7 +36,7 @@ function App() {
   useEffect(() => {
     const fetchTransactions = async () => {
       try {
-        const querySnapshot = await getDocs(collection(db, 'Transactions'));
+        const querySnapshot = await getDocs(collection(db, "Transactions"));
         const transactionsData = querySnapshot.docs.map((doc) => {
           return {
             ...doc.data(),
@@ -47,9 +47,9 @@ function App() {
         setTransactions(transactionsData);
       } catch (err) {
         if (isFireStoreError(err)) {
-          console.error('Firestoreエラー：', err);
+          console.error("Firestoreエラー：", err);
         } else {
-          console.error('一般的なエラー：', err);
+          console.error("一般的なエラー：", err);
         }
       }
     };
@@ -66,8 +66,8 @@ function App() {
     try {
       // firestoreにデータを保存
       // Add a new document with a generated id.
-      const docRef = await addDoc(collection(db, 'Transactions'), transaction);
-      console.log('Document written with ID: ', docRef.id);
+      const docRef = await addDoc(collection(db, "Transactions"), transaction);
+      console.log("Document written with ID: ", docRef.id);
 
       const newTransaction = {
         id: docRef.id,
@@ -80,9 +80,9 @@ function App() {
       ]);
     } catch (err) {
       if (isFireStoreError(err)) {
-        console.error('Firestoreエラー：', err);
+        console.error("Firestoreエラー：", err);
       } else {
-        console.error('一般的なエラー：', err);
+        console.error("一般的なエラー：", err);
       }
     }
   };
@@ -90,16 +90,16 @@ function App() {
   const handleDeleteTransaction = async (transactionId: string) => {
     // firestoreのデータ削除
     try {
-      await deleteDoc(doc(db, 'Transactions', transactionId));
+      await deleteDoc(doc(db, "Transactions", transactionId));
       // 表示したい削除されたid以外の取引
       const filteredTransactions = transactions.filter(
         (transaction) => transaction.id !== transactionId
       );
     } catch (err) {
       if (isFireStoreError(err)) {
-        console.error('Firestoreエラー：', err);
+        console.error("Firestoreエラー：", err);
       } else {
-        console.error('一般的なエラー：', err);
+        console.error("一般的なエラー：", err);
       }
     }
   };
@@ -109,7 +109,7 @@ function App() {
     transactionId: string
   ) => {
     try {
-      const docRef = doc(db, 'Transactions', transactionId);
+      const docRef = doc(db, "Transactions", transactionId);
 
       await updateDoc(docRef, transaction);
       const updatedTransactions = transactions.map((t) =>
@@ -118,9 +118,9 @@ function App() {
       setTransactions(updatedTransactions);
     } catch (err) {
       if (isFireStoreError(err)) {
-        console.error('Firestoreエラー：', err);
+        console.error("Firestoreエラー：", err);
       } else {
-        console.error('一般的なエラー：', err);
+        console.error("一般的なエラー：", err);
       }
     }
   };
@@ -143,7 +143,15 @@ function App() {
                 />
               }
             />
-            <Route path="/report" element={<Report />} />
+            <Route
+              path="/report"
+              element={
+                <Report
+                  currentMonth={currentMonth}
+                  setCurrentMonth={setCurrentMonth}
+                />
+              }
+            />
             <Route path="*" element={<NoMatch />} />
           </Route>
         </Routes>
--- src/components/MonthSelector.tsx
+++ src/components/MonthSelector.tsx
@@ -1,35 +1,59 @@
-import { Box, Button } from '@mui/material';
-import React from 'react';
-import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
-import { ja } from 'date-fns/locale';
+import { Box, Button } from "@mui/material";
+import React from "react";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
+import { ja } from "date-fns/locale";
+import { addMonths } from "date-fns";
 
-const MonthSelector = () => {
+interface MonthSelectorProps {
+  currentMonth: Date;
+  setCurrentMonth: React.Dispatch<React.SetStateAction<Date>>;
+}
+
+const MonthSelector = ({
+  currentMonth,
+  setCurrentMonth,
+}: MonthSelectorProps) => {
+  const handlePreviousMonth = () => {
+    const previousMonth = addMonths(currentMonth, -1);
+    setCurrentMonth(previousMonth);
+  };
+
+  const handleNextMonth = () => {
+    const nextMonth = addMonths(currentMonth, +1);
+    setCurrentMonth(nextMonth);
+  };
+
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ja}>
       <Box
         sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <Button color={'error'} variant="contained">
+        <Button
+          onClick={handlePreviousMonth}
+          color={"error"}
+          variant="contained"
+        >
           先月
         </Button>
         <DatePicker
+          value={currentMonth}
           label="年月を選択"
-          sx={{ mx: 2, background: 'white' }}
-          views={['year', 'month']}
+          sx={{ mx: 2, background: "white" }}
+          views={["year", "month"]}
           format="yyyy/MM"
           slotProps={{
             toolbar: {
-              toolbarFormat: 'yyyy年MM月',
+              toolbarFormat: "yyyy年MM月",
             },
-            calendarHeader: { format: 'yyyy年MM月' },
+            calendarHeader: { format: "yyyy年MM月" },
           }}
         />
-        <Button color={'primary'} variant="contained">
+        <Button onClick={handleNextMonth} color={"primary"} variant="contained">
           次月
         </Button>
       </Box>
